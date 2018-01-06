06e476143beff7fe0263d9f8762e622dde8ce636
D. Andrei Măceș <dmaces@nd.edu>
2018-01-06 18:17:09 +0200

    audio: Migrate to XML audio policy configuration
    Partially inspired by:
    https://android.googlesource.com/device/lge/bullhead/+/b1d6440e7
    
    While at it, use TARGET_COPY_OUT_VENDOR with all changed paths.
    
    Change-Id: I6a0ef23b8a366da66a99713a9bf4c180448e18e1
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- msm8916.mk
+++ msm8916.mk
@@ -92,8 +92,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	vendor.voice.path.for.pcm.voip=true
 
 PRODUCT_COPY_FILES +=  \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
