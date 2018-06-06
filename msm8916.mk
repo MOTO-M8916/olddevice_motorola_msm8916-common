c289f7fb297a746938c4c2bc6e52f88133bba1ee
althafvly <althafvly@gmail.com>
2018-06-06 17:19:39 +0000

    msm8916-common: Remove current GPS Implementation

--- msm8916.mk
+++ msm8916.mk
@@ -195,13 +195,7 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service \
-    gps.msm8916 \
     libshims_get_process_name
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.gps.agps_provider=1 \
