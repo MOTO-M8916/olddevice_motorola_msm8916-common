3e354f1517572c96f226cc5fa880ea18d9cb63a2
Ethan Chen <intervigil@gmail.com>
2019-01-13 18:44:25 -0500

    msm8916-common: provide fake build.prop for Morpho library DRM
    Some camera libraries (such as Morpho HDR) try to open /system/build.prop
    and check that it contains `ro.product.manufacturer=Motorola`. On Oreo,
    the build.prop is readable only by root, so this fails. These libraries
    deliberately crash in strange ways when they fail to read build.prop.
    To work around these, I hex edited the libraries to use `/vendor/hacks.prop`
    (that I created as world readable) instead of `/system/build.prop`.
    
    Credits to Ethan Chen (intervigil) for originally discovering this behaviour
    on HTC devices.
    
    Change-Id: I1f8ce7aff6d11229b2acb1a7e81d643aa5829a39

--- msm8916.mk
+++ msm8916.mk
@@ -133,7 +133,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera configurations
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+	$(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml \
+	$(LOCAL_PATH)/configs/hacks.prop:$(TARGET_COPY_OUT_VENDOR)/hacks.prop
 
 # Charger
 PRODUCT_PACKAGES += \
